b95fa59c4f1d69137c78489b7148a18b4b1a8794
uladzislauandrashyn <andrashyn@gmail.com>
2022-03-08 03:43:02 +0300

    HW_1

---
+++ src/main/java/andr1/lec_1/classwork/point_4/JavaTypes.java
@@ -0,0 +1,36 @@
+package andr1.lec_1.classwork.point_4;
+
+public class JavaTypes {
+    public static void main(String[] args) {
+        // 0 12 33
+        /*
+        byte 2^8 -> 2^7-1 -> -128 +127
+        short 2^16
+        char 2^16-1 -> max 65535
+        long 2^64
+         */
+        // 0.1234
+        // true/false
+
+        // byte, short, char, long
+        // float, double
+        // boolean
+
+        // String -> "LetMeFly"
+
+        // byte age = 0; переменная типа "Байт" называется "age" и равна нулю
+
+        byte age = 127;
+
+        String name = "LetMeFly";
+
+        char myChar = '0';
+        char mySymbolChar = 'Y';
+
+        System.out.println(myChar);
+        System.out.println(mySymbolChar);
+
+
+
+    }
+}
