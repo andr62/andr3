1c23c41e282aab6e280a6bc2fc64fd7d71e8f830
uladzislauandrashyn <andrashyn@gmail.com>
2022-03-13 13:27:54 +0300

    mac2pc

--- src/main/java/andr/lec_2/homework/task_1/RunningMan.java
+++ src/main/java/andr/lec_2/homework/task_1/RunningMan.java
@@ -1,2 +1,10 @@
-package andr.lec_2.homework.task_1;public class RunningMan {
+package andr.lec_2.homework.task_1;
+
+public class RunningMan {
+    public static void main(String[] args) {
+        for (int distance = 0; distance = 11; distance = distance*10%; ) {
+
+        }
+
+    }
 }
