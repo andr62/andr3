a492b2550bceb45d0685f0ee7c604f036b1b7cff
Uladzislau Andrashyn <andrashyn@gmail.com>
2022-03-20 22:48:47 +0300

    20032022

--- src/main/java/andr/lec_2/homework/task_1/RunningMan.java
+++ src/main/java/andr/lec_2/homework/task_1/RunningMan.java
@@ -2,11 +2,14 @@ package andr.lec_2.homework.task_1;
 
 public class RunningMan {
     public static void main(String[] args) {
-        int day1 = 10;
-
-
-
+        double zeroDay = 10;
+        double everyDay = zeroDay;
+        double totalTrack = everyDay + zeroDay;
+        for (double days = 1; days < 8; days++) {
+            everyDay = zeroDay + zeroDay / 10;
+            totalTrack = totalTrack + everyDay;
+        }
+System.out.println("OverallDist: " + totalTrack + " km");
 
     }
-
 }
---
+++ src/main/java/andr/lec_2/homework/task_4/Multiply.java
@@ -0,0 +1,10 @@
+package andr.lec_2.homework.task_4;
+
+public class Multiply {
+    public static void main(String[] args) {
+int a = 1;
+int b = 2;
+boolean isNegative = false;
+
+    }
+}
