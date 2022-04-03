2f72781c957fb42e7cffbd9dcd6e7621fd100f16
Uladzislau Andrashyn <andrashyn@gmail.com>
2022-04-03 23:21:15 +0300

    aprilDZ

---
+++ src/main/java/andr/lec_3/homework/task_1/ScannerHome.java
@@ -0,0 +1,17 @@
+package andr.lec_3.homework.task_1;
+
+import java.util.Scanner;
+
+public class ScannerHome {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        for (int i = 0; i < array.length; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your number: ");
+            int numberFromInput1 = scanner.nextInt();
+            String name = scanner.next();
+  //          int scannerInput = Scanner(System.in);
+   //         int scannerInput2 = Scanner(System.in);
+        }
+    }
+}
--- src/main/java/andr/lec_3/homework/task_1/ThreeElementArray.java
+++ src/main/java/andr/lec_3/homework/task_1/ThreeElementArray.java
@@ -1,15 +1,25 @@
 package andr.lec_3.homework.task_1;
 
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ThreeElementArray {
     public static void main(String[] args) {
-        int[] array = new int[6];
+
+        int[] array = new int[3];
         for (int i = 0; i < array.length; i++) {
-            array[i] = i + 0;
+            array[i] = i + 1;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter you number: ");
+            String name = scanner.next();
+            System.out.println(Arrays.toString(array));
+            // System.out.println(Arrays.toString(i * i if i < 2 ));
         }
-        System.out.println(array);
+        // System.out.println(array);
+      //  InputStream = System.in;
         System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(array));
-    }
+        System.out.println("The sum of their choices will be: " + System.in);
+
+}
 }
