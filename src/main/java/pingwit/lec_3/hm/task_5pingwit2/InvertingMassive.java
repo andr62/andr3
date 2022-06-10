2f21c1664df653176469208d40efeb1d19bab94e
Mary Stankevich <marrycitty@mail.ru>
2022-06-10 15:43:31 +0200

    Update InvertingMassive.java

--- src/main/java/pingwit/lec_3/hm/task_5pingwit2/InvertingMassive.java
+++ src/main/java/pingwit/lec_3/hm/task_5pingwit2/InvertingMassive.java
@@ -2,6 +2,7 @@ package pingwit.lec_3.hm.task_5pingwit2;
 
 import java.util.Arrays;
 
+//+
 public class InvertingMassive {
     public static void main(String[] args) {
         int[] array = new int[9];
@@ -9,17 +10,17 @@ public class InvertingMassive {
             array[i] = i;
         }
         System.out.println(Arrays.toString(array));
-
+//Лишняя пустая строка
 
         for (int i = 0; i < array.length / 2; i++) {
-            int temp = array.length - 1 - i;
-            int reversed = array[i];
+            int temp = array.length - 1 - i; //больше похоже на индекс симметричного элемента
+            int reversed = array[i]; //больше похож на temp
             array[i] = array[temp];
             array[temp] = reversed;
         }
 
     System.out.println(Arrays.toString(array));
-
+//Лишняя пустая строка
     }
 }
 
