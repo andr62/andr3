4119fa0fb2cfb1fc51b2642d14002f2fa1ba1927
Mary Stankevich <marrycitty@mail.ru>
2022-06-10 15:40:56 +0200

    Update MultiplicationTable.java

--- src/main/java/pingwit/lec_3/hm/task_4pingwit2/MultiplicationTable.java
+++ src/main/java/pingwit/lec_3/hm/task_4pingwit2/MultiplicationTable.java
@@ -1,9 +1,10 @@
 package pingwit.lec_3.hm.task_4pingwit2;
 
+//+
 public class MultiplicationTable {
     public static void main(String[] args) {
-//        int tableSize = 5;
-        int[][] array = new int[11][11];
+//        int tableSize = 5; <- удаляем ненужные комментарии 
+        int[][] array = new int[11][11]; //попробуй сделать через массив размера 9х9 
 
         for (int a = 1; a < array.length; a++) {
             for (int b = 1; b < array[a].length; b++) {
