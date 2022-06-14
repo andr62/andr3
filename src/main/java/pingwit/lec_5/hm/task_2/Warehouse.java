907deb532ecfdbe8316bf2681a2173ef933c3368
Uladzislau Andrashyn <andrashyn@gmail.com>
2022-06-14 23:54:14 +0300

    workingHard

--- src/main/java/pingwit/lec_5/hm/task_2/Warehouse.java
+++ src/main/java/pingwit/lec_5/hm/task_2/Warehouse.java
@@ -1,14 +1,10 @@
-//package pingwit.lec_5.hm.task_2;
-//
-//public class Warehouse {
-//
-//    private static Integer ID;
-//    private static String brand;
-//
-//    //final String[] CarsQuantity = new String[5];
-//
-//    public Car(Integer ID, String brand) {
-//        Car.ID = ID;
-//        String brand = brand;
+package pingwit.lec_5.hm.task_2;
+
+public class Warehouse implements Car {
+//    public Warehouse(Warehouse[] cars) {
+//        CarsAvailable = cars;
 //    }
-//}
+
+}
+
+
--- src/main/java/pingwit/lec_5/hm/task_2/WarehouseMain.java
+++ src/main/java/pingwit/lec_5/hm/task_2/WarehouseMain.java
@@ -3,5 +3,17 @@ package pingwit.lec_5.hm.task_2;
 public class WarehouseMain {
     public static void main(String[] args) {
 
+//        @Override
+//        public boolean carsLeftInTheWarehouse() {
+//            return Car.super.carsLeftInTheWarehouse();
+//        }
+        Car firstCar = new Car() {
+        };
+
+        if (firstCar.carsLeftInTheWarehouse()) {
+            System.out.println("Test");
+        }
+
+
     }
 }
